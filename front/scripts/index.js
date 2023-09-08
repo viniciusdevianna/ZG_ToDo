7c33b313b23e6a89c92efc036876c565699cf4fb
devianna <vinidevianna@gmail.com>
2023-09-08 18:24:39 -0300

    feat: adicionado o campo de prioridade

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,4 +1,4 @@
-let tasks = ["Tarefa 1", "Tarefa 2", "Tarefa 3"];
+let tasks = [];
 
 function updateTaskList() {
     const taskList = document.getElementById("tasklist");
@@ -7,17 +7,28 @@ function updateTaskList() {
     tasks.forEach((task) => {
         let li = document.createElement("li");
         li.className = "task-card"
-        li.innerHTML = task;
+        li.id = "taskcard"
+        li.innerHTML = `<p>${task.name}</p><p>${task.priority}</p>`
         taskList.appendChild(li);
     });
 }
 
 const addTaskBtn = document.getElementById("addTaskBtn");
 const newTaskName = document.getElementById("newTaskName");
+const prioritySelector = document.getElementById("prioritySelector");
 
 addTaskBtn.onclick = () => {
     if (newTaskName.value) {
-        tasks.push(newTaskName.value);
+        let newTask = {
+            id: tasks.length,
+            name: newTaskName.value,
+            priority: prioritySelector.value,
+            description: "",
+            category: "",
+            status: "ToDo",
+            limitDate: null
+        }
+        tasks.push(newTask);
         updateTaskList();
         newTaskName.value = "";
     }   
