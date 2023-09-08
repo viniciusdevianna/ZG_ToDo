2409df12fb32c8ead4686ba90925165d99bad4db
devianna <vinidevianna@gmail.com>
2023-09-08 19:46:31 -0300

    feat: tarefas agora são clicáveis. Melhorei também o menu principal e o card de tarefa

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,4 +1,11 @@
 let tasks = [];
+const priorities = {
+    1: "&#9675;",
+    2: "&#128994;",
+    3: "&#128993;",
+    4: "&#128308;",
+    5: "&#10071;&#10071;"
+};
 
 function updateTaskList() {
     const taskList = document.getElementById("tasklist");
@@ -6,9 +13,21 @@ function updateTaskList() {
     taskList.innerHTML = "";
     tasks.forEach((task) => {
         let li = document.createElement("li");
-        li.className = "task-card"
-        li.id = "taskcard"
-        li.innerHTML = `<p>${task.name}</p><p>${task.priority}</p>`
+        li.className = "task-card";
+        li.id = "taskcard";
+        li.innerHTML = 
+            `<div class="card-title">
+                <div class="card-name">
+                <p>${task.name}</p>
+                <p>${priorities[task.priority]}</p>
+                </div>
+                <p>${task.status}</p>
+            </div>
+            <div class="card-description">
+                <p>${task.description}</p>
+                <p>${task.limitDate || ""}</p>
+            </div>`;
+        li.onclick = () => location.href = "./edit.html";
         taskList.appendChild(li);
     });
 }
@@ -23,7 +42,7 @@ addTaskBtn.onclick = () => {
             id: tasks.length,
             name: newTaskName.value,
             priority: prioritySelector.value,
-            description: "",
+            description: "Descrição",
             category: "",
             status: "ToDo",
             limitDate: null
