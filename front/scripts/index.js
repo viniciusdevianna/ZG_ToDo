29b87f33f6272e13cc9f1fecde0d0e82a7d22e5c
viniciusdevianna <vinidevianna@gmail.com>
2023-09-10 23:28:55 -0300

    feat: implementada a possibilidade de deletar tarefas

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,3 +1,5 @@
+// TODO: refatorar este código para separar as funções
+
 let tasks = [];
 const priorities = {
     1: "&#9675;",
@@ -16,12 +18,15 @@ function updateTaskList() {
         li.className = "task-card";
         li.id = "taskcard";
         li.innerHTML =
-            `<div class="card-title">
+            `<div class="card-title" id=taskCard${task.id}>
                 <div class="card-name">
                 <p>${task.name}</p>
                 <p>${priorities[task.priority]}</p>
                 </div>
                 <p>${task.status}</p>
+                <a id="deleteIcon${task.id}">
+                    <span class="material-icons">delete</span>
+                </a>
             </div>
             <div class="card-description">
                 <p>${task.description}</p>
@@ -29,6 +34,13 @@ function updateTaskList() {
             </div>`;
         li.onclick = () => location.href = "./edit.html";
         taskList.appendChild(li);
+        document.getElementById(`deleteIcon${task.id}`).onclick = (event) => {
+            event.stopPropagation();
+            if (confirm("Tem certeza que deseja deletar a tarefa?")) {
+                tasks = tasks.filter((t) => t !== task);
+                updateTaskList();
+            }
+        }
     });
 }
 
